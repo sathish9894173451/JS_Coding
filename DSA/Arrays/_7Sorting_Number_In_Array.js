fbc9d394d73baaa6085f464230b86eae33924634
dharunr <dharun.r.partner@decathlon.com>
2026-03-22 22:57:27 +0530

    rearranged_folder

--- DSA/Arrays/_7Sorting_Number_In_Array.js
+++ DSA/Arrays/_7Sorting_Number_In_Array.js
@@ -1,25 +1,26 @@
 // 7.Sorting in array
-const numbers = [10,4,8,2,15,28,56];
+const numbers1 = [10,4,8,2,15,28,56];
 
-// const sorting = numbers.sort((a,b) =>{
-//     return a-b;
-// })
-// console.log("sorting", sorting)
+const sorting = numbers1.sort((a,b) =>{
+    return a-b; //asc order a-b, desc order b-a
+})
+console.log("sorting", sorting)
 // without inbuilt function using bubble sort
-function bubbleSort(numbers){
+const numbers = [10,4,8,2,15,28,56];
+function sortingNumber(numbers){
 for(let i=0; i < numbers.length -1;i++){
     for(let j=0; j < numbers.length - 1 - i; j++){ //(7 - 1 - 0) //until (7 - 1 - 6)
         if(numbers[j] > numbers[j+1]){
             // swap
         let temp = numbers[j];
         numbers[j] = numbers[j+1];
-        numbers[j+1] = temp
+        numbers[j+1] = temp;
     }
 }
 }
-return numbers
+console.log("Final Sorted Array:", numbers);
 }
-console.log(bubbleSort(numbers))
+sortingNumber(numbers);
 
 // if you want 1st element comes first like [10, 15, 2, 28,4, 56, 8] then use simple sort.It works as ASCII
 // const sorting = numbers.sort()
